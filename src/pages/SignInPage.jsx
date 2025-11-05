1a87b1cb23a5f43304be82eb06de53007cba9304
Joshua Igho <ighojoshua2024@gmail.com>
2025-11-05 16:14:44 +0100

    redeploy frontend

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react";
 
 export default function SignInPage() {
   return (
-    <div style={{ display: "flex", justifyContent: "center", padding: "2rem" }}>
+    <div style={{ display: "flex", justifyContent: "center", padding: "3rem" }}>
       <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up" />
     </div>
   );
